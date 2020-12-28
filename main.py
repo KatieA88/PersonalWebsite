d591c7fb37a1d22f40f797c90853ba8b0f387c26
Katie Adams <katieadams@Katies-MacBook-Pro.local>
2020-12-28 15:52:45 +0000

    updated with content for page

--- main.py
+++ main.py
@@ -14,6 +14,7 @@
 
 # [START gae_python38_app]
 from flask import Flask, render_template
+import json
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
@@ -23,13 +24,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
+    with open('static/data/project.json') as json_file:
+        data = json.load(json_file)
   
-     return render_template('index.html')
+    return render_template('index.html', projects = data['projects'])
 
+@app.route('/about')
+def aboutroot():
+  
+    return render_template('about.html')
+
+@app.route('/contact')
+def contactroot():
+  
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8000, debug=True)
 # [END gae_python38_app]
